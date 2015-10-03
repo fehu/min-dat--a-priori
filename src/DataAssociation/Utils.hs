20c5fde0d35e209286261c7953b62936c34d2879
fehu <lord.fehu@gmail.com>
2015-10-02 20:11:23 -0500

    added documentation for Utils;

--- src/DataAssociation/Utils.hs
+++ src/DataAssociation/Utils.hs
@@ -1,3 +1,10 @@
+{- |
+
+Module      : DataAssociation.Utils
+Description : Different utilities.
+License     : MIT
+Stability   : development
+-}
 module DataAssociation.Utils (
 
   preservingArg
@@ -24,19 +31,28 @@ import DataAssociation.Definitions
 -- I guess in Haskell should already exist a way to do it,
 -- but I don't know it
 
+-- | Preserves an argument and returns it, tupled with function's result
+--
+-- > preservingArg id x == (x, x)
 preservingArg :: (a -> b) -> a -> (a, b)
 preservingArg f a = (a, f a)
 
--- http://stackoverflow.com/questions/15412027/haskell-equivalent-to-scalas-groupby
-sortingGroupBy :: (Ord b) => (a -> b) -> ([a] -> c) -> [a] -> [(b, c)]
+-- | Groups the elements of a list according to /discriminator function/
+--    and applies /result function/ to each formed group
+--
+-- see <http://stackoverflow.com/questions/15412027/haskell-equivalent-to-scalas-groupby>
+sortingGroupBy :: (Ord b) => (a -> b)   -- ^ /discriminator function/
+                          -> ([a] -> c) -- ^ /result function/
+                          -> [a]        -- ^ a list
+                          -> [(b, c)]   -- result of grouping and /result function/ application
 sortingGroupBy f g = map (f . head &&& g)
                          . groupBy ((==) `on` f)
                          . sortBy (compare `on` f)
 
 
 -----------------------------------------------------------------------------
--- given an itemset of length l, returns a set of itemsets of length (l - 1)
---                                      that are subsets of the original one
+-- | given an itemset of length l, returns a set of itemsets of length (l - 1)
+--                                       that are subsets of the original one
 allSubsetsOneShorter :: (Itemset set it) => set it -> [set it]
 allSubsetsOneShorter set = liftM (`deleteItemAt` set) [0 .. setSize set - 1]
 -- this equals to:
@@ -45,8 +61,8 @@ allSubsetsOneShorter set = liftM (`deleteItemAt` set) [0 .. setSize set - 1]
 
 
 -----------------------------------------------------------------------------
--- count the number of occurences of each set in the transactions
--- the 'occurence' is when the set is a subset of the transaction
+-- | count the number of occurences of each set in the transactions
+--   the 'occurence' is when the set is a subset of the transaction
 countSupported :: (Ord (set it), Itemset set it) =>
     [set it] -> [set it] -> Map (set it) Int
 
@@ -58,9 +74,16 @@ countSupported transactions sets = Map.fromList cl
 
 
 
-
+calculateSupport :: Int     -- ^ total number of transactions
+                 -> Int     -- ^ number of occurences
+                 -> Float   -- ^ support
 calculateSupport transactionsSize = (/ int2Float transactionsSize) . int2Float
 
+
+sufficientSupport :: MinSupport
+                  -> Int    -- ^ total number of transactions
+                  -> Int    -- ^ number of occurences
+                  -> Bool   -- ^ has sufficient support?
 sufficientSupport (MinSupport minsup) transactionsSize =
     (>= minsup) . calculateSupport transactionsSize
 
