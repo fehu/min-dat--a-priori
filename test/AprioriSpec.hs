17f1ea3a85f8071dd4e694f1beaad8f4e67a52dd
fehu <lord.fehu@gmail.com>
2015-10-05 00:30:36 -0500

    comparative output of example's and result's runs;

--- test/AprioriSpec.hs
+++ test/AprioriSpec.hs
@@ -10,6 +10,7 @@ module AprioriSpec ( spec ) where
 import Test.Hspec
 
 import Text.Printf
+import Data.List
 
 import AprioriSpec.Data
 import DataAssociation.Definitions
@@ -21,7 +22,7 @@ spec :: (Itemset set it, Ord (set it), Ord it) =>
 
 spec ex =
     describe "DataAssociation.APriori" $
-    context "given a precalculated example" $
+    context  "given a precalculated example" $
     describe "must behave as the example given" $ sequence_ $ matchWithExample ex
 
 
@@ -29,14 +30,26 @@ spec ex =
 matchWithExample :: (Itemset set it, Ord (set it), Ord it) =>
     AprioriTestData set it -> [Spec]
 
-matchWithExample (AprioriTestData transactions minsup minconf runs) = testNRuns:testRuns
+matchWithExample (AprioriTestData transactions minsup minconf runs) = beforeAll printDD testNRuns : testRuns
     where (res, ddata) = runApriori minsup transactions
           testNRuns = it "has the same number of runs" $ length runs `shouldBe` length ddata
-          testRuns  = do ( AprioriDebugData seeds joined pruned , AprioriDebugData seeds' joined' pruned', i) <- zip3 runs ddata [1..]
+          testRuns  = do (AprioriDebugData seeds joined pruned, AprioriDebugData seeds' joined' pruned', i) <- zip3 runs ddata [1..]
                          [
-                            it (printf "receives the same seeds  L_{%s}" (show i))     $ seeds  == seeds'
-                          , it (printf "produces the same joined L_{%s}" (show $ i+1)) $ joined == joined'
-                          , it (printf "produces the same pruned L_{%s}" (show $ i+1)) $ pruned == pruned'
+                            it (printf "receives the same seeds  L_{%s}" (show i))     $ sort seeds  == sort seeds'
+                          , it (printf "produces the same joined L_{%s}" (show $ i+1)) $ sort joined == sort joined'
+                          , it (printf "produces the same pruned L_{%s}" (show $ i+1)) $ sort pruned == sort pruned'
                           ]
+          printDD = sequence_ $
+            do (AprioriDebugData seeds joined pruned, AprioriDebugData seeds' joined' pruned', i) <- zip3 runs ddata [1..]
+               return $ do putStrLn $ "On Run " ++ show i ++ ":\n"
+                           putStrLn $ "Seeds  (Example): " ++ show seeds
+                           putStrLn $ "Seeds  (Result) : " ++ show seeds'
+                           putStrLn ""
+                           putStrLn $ "Joined (Example): " ++ show joined
+                           putStrLn $ "Joined (Result) : " ++ show joined'
+                           putStrLn ""
+                           putStrLn $ "Pruned (Example): " ++ show pruned
+                           putStrLn $ "Pruned (Result) : " ++ show pruned'
+                           putStrLn "\n"
 
 
