f955c37e3a365a455a8f4c0c48551509761cc412
fehu <lord.fehu@gmail.com>
2015-10-06 00:26:01 -0500

    version 0.2.0; mine-rules executable now acceptes parameters from command line;

--- src/Main.hs
+++ src/Main.hs
@@ -2,8 +2,50 @@ module Main (
     main
 ) where
 
-import DataAssociationTest
+import RunDataAssocWekaApriorySimple
+import DataAssociation
+
+import System.Environment
+import System.Exit
+import System.IO
+import Data.Maybe
+import Control.Monad
 
 main :: IO ()
-main = run
+main = getArgs >>= parse
 
+data ParsedArg = ParsedArg { filename :: String
+                           , minsup   :: Float
+                           , minconf  :: Float
+                           }
+
+
+maybeRead = fmap fst . listToMaybe . reads
+
+maybeReadInUnit :: String -> Maybe Float
+maybeReadInUnit s = (maybeRead s :: Maybe Float) >>= f
+            where f x | x <= 1 && x >= 0 = Just x
+                      | otherwise        = Nothing
+
+parse ["-h"] = usage >> exitSuccess
+parse [fname, msup, mconf] = do
+    let minsup  = maybe minsupError  MinSupport    $ maybeReadInUnit msup
+    let minconf = maybe minconfError MinConfidence $ maybeReadInUnit mconf
+--    if not . doesFileExist $ fname then error $ "File " ++ fname ++ "not found"
+--                                   else mzero
+    run fname minsup minconf
+parse _ = unknownCmd >> usage >> exitFailure
+
+--parse [fname, msup, mconf] | (maybeRead msup :: Maybe Float) <= 1 = putStrLn ""
+
+
+
+unknownCmd = putStrLn "Wrong arguments!"
+usage = do putStrLn "Usage: mine-rules [-h] file MinSupport MinConfidence"
+           putStrLn "       where file is an *.arff nominal data file"
+           putStrLn "             MinSupport and MinConfidence must be Float values in [0, 1]"
+
+
+minsupError  = boundError "MinSupport"
+minconfError = boundError "MinConfidence"
+boundError nme = error $ nme ++ " must be a Float in [0, 1]"
---
+++ src/RunDataAssocWekaApriorySimple.hs
@@ -0,0 +1,33 @@
+{- |
+
+Module      : RunDataAssocWekaApriorySimple
+Description : Runs data association.
+License     : MIT
+Stability   : development
+
+Uses "DataAssociation.APriori" and "DataAssociation.SimpleRulesGenerator"
+    to generate /data association rules/ from /*arff/ __nominal__ data.
+
+-}
+
+module RunDataAssocWekaApriorySimple ( run ) where
+
+import WekaData
+import DataAssociation
+import DataAssociation.Itemset.SetImpl
+import DataAssociation.APriori
+import DataAssociation.SimpleRulesGenerator
+
+type Filename = String
+
+--
+run :: Filename -> MinSupport -> MinConfidence -> IO ()
+run filename minsup minconf =
+    do rawData <- readWekaData filename
+       let sparse = wekaData2Sparse rawData
+       let itemsets = map newItemset sparse :: Itemsets String
+
+       let rules = mineAssociationRules minsup minconf itemsets
+
+       sequence_ $ do rule <- rules
+                      return $ print rule
